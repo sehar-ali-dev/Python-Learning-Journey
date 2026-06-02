c94ced9c6ae03513032013785eaa2ee54d8982ef
Sehar Ali <behaviorexplorehuman@gmail.com>
2026-06-02 22:54:47 +0500

    Fixed input type casting with int and float
    - Cleared concepts regarding string input behaviors and concatenation pitfalls.
    - Refactored calculator inputs using int() and float() functions for accurate numerical operations.
    - Removed invalid NoneType and Boolean functions from input conversion lines to prevent runtime errors.
    - Added clarity regarding variable reassignment versus data type immutability in Python.

---
+++ Python_Revision/Day_6_Take_use_input.py
@@ -0,0 +1,34 @@
+a = input()
+print("I am learning python.")
+
+
+a = input("enter your name: ")
+print("My name is:", a)
+
+#ab yaha pr ziyadah tr doubt hota h 
+
+x = input("enter your first number: ")
+y = input("Enter your second number:")
+print (x+y)
+
+#yaha pr string h ab hum type casting pry gay "int" lgain gay 
+#mtl k yaha pr hum ny varivale ki data type cge ki 
+#bcz variables are mutable mtlb k change ho skty h 
+#lekin Python mein int, float, str khud Immutable (un-changeable) hote hain.
+
+
+#yaha pr all 5 data types use ki h 
+
+x = (int(input("enter your first number:")))
+y = (int(input("Enter your second number:")))
+print("the sum of x and y is:", x+y)
+
+x = (float(input("enter your first number:")))
+y = (float(input("Enter your second number:")))
+print("the sum of x and y is:", x+y)
+
+
+
+
+
+
