30c888943f5b48c0cbf3cbe4e484be61672f0e73
Sehar Ali <behaviorexplorehuman@gmail.com>
2026-06-06 18:48:43 +0500

    Day 14 Exercise 2: "Current Time Extractor"

---
+++ Python_Revision/Day 14_Exercise 2.py
@@ -0,0 +1,11 @@
+import time
+current_time = (time.strftime('%H:%M:%S'))
+print("Full time:", current_time )
+Hour = int(time.strftime('%H'))
+print("Hour:", Hour)
+Min = int(time.strftime("%M"))
+print("Min:", Min)
+Sec = int(time.strftime("%S"))
+print("sec:",Sec)
+
+
