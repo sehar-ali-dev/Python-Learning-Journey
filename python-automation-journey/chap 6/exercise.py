86d394a8db6668f9c0e84652c35b2d2506011c54
Sehar Ali <behaviorexplorehuman@gmail.com>
2026-06-08 00:00:16 +0500

    While loops _condition

---
+++ python-automation-journey/chap 6/exercise.py
@@ -0,0 +1,5 @@
+num= int(input("Enter a number = "))
+i=1
+while i<12:
+    print(num , "x",  i , "=" , num*i)
+    i +=1
---
+++ python-automation-journey/chap 6/while loops.py
@@ -0,0 +1,8 @@
+#counter variable
+i=1
+while i<6:
+    if i==3:
+        print(i)
+        break
+    print(i)
+    i=i+1 
