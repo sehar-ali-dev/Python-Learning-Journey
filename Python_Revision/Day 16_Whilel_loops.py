fb3db6e2042aad261345d90ecf72f3bf327f798e
Sehar Ali <behaviorexplorehuman@gmail.com>
2026-06-08 00:56:00 +0500

    Day 16_While loop condition

---
+++ Python_Revision/Day 16_Whilel_loops.py
@@ -0,0 +1,15 @@
+i = int(input("Enter the number:"))
+while i<=37:
+    i=int(input("Enter the number:"))
+    print(i)
+print("done with the loop")
+
+
+#second example
+count = 5 
+while (count > 0):
+    print(count)
+    count = count - 1
+#while loop k sath else ka bhi istamal kiya ja skta h 
+else:
+    print("i am inside else")
