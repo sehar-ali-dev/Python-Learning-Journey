cfb9cf0c443cd4a2c4d20031d83a622ebd884c5a
Sehar Ali <behaviorexplorehuman@gmail.com>
2026-06-09 23:39:09 +0500

    Break & Continue

---
+++ Python_Revision/Day 17_Break&continue.py
@@ -0,0 +1,29 @@
+# break
+
+for i in range(12):
+    print("5 x", i, "=", 5*(i))
+    if (i==10):
+     break 
+    
+print("loop ko chor ko nikal gaya.")
+
+
+# Second example of break
+for i in range(12):
+    
+    if (i==10):
+     break 
+    print("5 x", i, "=", 5*(i))
+print("loop ko chor ko nikal gaya.")
+
+#continue
+for i in range(12):
+    
+    if (i==10):
+     print("skip the iteration")
+     continue
+    print("5 x", i, "=", 5*(i))
+
+
+#do while loop learn bhi learn krnah 
+
