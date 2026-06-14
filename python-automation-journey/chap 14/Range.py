d291e43ec76848e820d1d24f3ba9d5e77898d2d7
Sehar Ali <behaviorexplorehuman@gmail.com>
2026-06-14 17:39:32 +0500

    Range

---
+++ python-automation-journey/chap 14/Range.py
@@ -0,0 +1,30 @@
+#range have 3 perimeters
+#range(start,stop,step)
+#agr range mn single value ko pass kry gay vo stop value hoti hai 
+x=range(10)
+print(x)
+print(list(x))
+
+#es mn do perimeters pass kiye hai 
+x=range(1,10)#yaha pr pehli value start ko show krti h 
+print(x)
+print(list(x))
+
+
+x=range(1,10,2)#yaha pr pehli value third  ko show krti h yani ye step ka perimeter h 
+print(x)
+print(list(x))
+
+
+#yaha pr ak countdown  bnaie gay 
+
+
+#for i in range(1,20,2):
+  #  print(i)
+for i in range(20,0,-1):
+    print(i)
+
+
+
+
+
