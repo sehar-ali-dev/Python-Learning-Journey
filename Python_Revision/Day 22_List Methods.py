31c7c8c1961993f704e3586d979f2f28a69e7d7d
Sehar Ali <behaviorexplorehuman@gmail.com>
2026-06-22 18:09:01 +0500

    List Methods Learning

---
+++ Python_Revision/Day 22_List Methods.py
@@ -0,0 +1,25 @@
+# List methods 
+l = [12 ,26, 34, 45,3, 4, 12, 54]
+print(l)
+#l.append(7) #append method 
+#l.sort()# sort method 
+#l.sort(reverse = True)# also a sort method 
+#l.reverse()# ye method orignal list ko ulta kr deta h 
+#print(l.index(26))
+#print(l.count(12))#count method k value kitni bar a h
+#copy method
+
+# m = l.copy()
+# m[0] = 0 
+# insert method 
+# l.insert(1,566)
+# mew list method again
+m = [800, 1000, 1100]
+# l.extend(m)
+# joint method 
+k = l+m
+print(k)
+
+print(l)
+
+
