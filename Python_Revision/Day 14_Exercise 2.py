d464cd73637fd962d0e86e2099c492202cdf495a
Sehar Ali <behaviorexplorehuman@gmail.com>
2026-06-25 21:33:29 +0500

    Enhance time display with greeting based on hour
    Added a method to greet based on user input hour.

--- Python_Revision/Day 14_Exercise 2.py
+++ Python_Revision/Day 14_Exercise 2.py
@@ -9,3 +9,30 @@ Sec = int(time.strftime("%S"))
 print("sec:",Sec)
 
 
+# one method more 
+import time
+
+t = time.strftime('%H:%M:%S')
+
+hour = int(time.strftime('%H'))
+
+hour = int(input("Enter hour:" ))
+
+print(hour)
+
+if(hour>=0 and hour<12):
+
+    print("Good Morning Mam")
+
+elif(hour>=12 and hour<17):
+
+    print("Good Afternoon! Mam")
+
+elif(hour>=17 and hour<=23):
+
+    print("Good night Mam")
+
+else:
+
+    print("Have a nice day.")
+
