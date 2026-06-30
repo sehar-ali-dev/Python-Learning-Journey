98bee424d9aea7415e9732d8e9e247f7fb4618db
Sehar Ali <behaviorexplorehuman@gmail.com>
2026-06-30 21:38:55 +0500

    f string method in python

---
+++ Python_Revision/Day 26_f - strings.py
@@ -0,0 +1,8 @@
+# string formating 
+letter = "hye my nmae is {} and i am from {}"
+country = "Pakistan"
+name = "Sehar Ali"
+#   .format() isa string method
+print(letter.format(name, country))
+# f string method
+print(f"hye my name is {name} and i am from {country}.")
