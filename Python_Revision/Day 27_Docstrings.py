6aab12b16c3b7eb3cab2c1bc809949f2e8fdb01a
Sehar Ali <behaviorexplorehuman@gmail.com>
2026-07-01 17:47:11 +0500

    Doc string and PEP 8, THE ZEN Python

---
+++ Python_Revision/Day 27_Docstrings.py
@@ -0,0 +1,11 @@
+# docstring and pep8 
+#  ye string function ko sahi sy samjhny k liye madad krti h 
+def square(n): 
+    '''take in a number n, returns the square of n''' #not a commet it is a docstring
+    print(n**2)
+square(5)
+print(square.__doc__)
+
+# PEP 8
+# PEP 8 is the official style guide for Python code. It provides conventions for writing readable and consistent Python code.
+import this #import this krny sy The Zen of python poem ati h 
