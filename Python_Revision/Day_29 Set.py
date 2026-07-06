1b002d408857a6691eb44ca1dd50c38ec5288aa0
SeharAli <behaviorexplorehuman@gmail.com>
2026-07-06 18:02:04 +0500

    Set in Python

---
+++ Python_Revision/Day_29 Set.py
@@ -0,0 +1,14 @@
+s = {2, 4, 2 ,6}
+print(s)
+# set rpeared value nahi deta 
+# same method use of list 
+# set are unordered 
+# set are unchangeable
+sehar = {} # called dicr not a set 
+print(type(sehar))
+# empty set esy bnaie gay 
+Sehar2 = set() #esy empty set bny ga 
+print(type(Sehar2))
+# use for loop 
+for value in s:
+    print(value)
